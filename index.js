3d0861578dea1cc3360b7cbbbc1b747faee481a4
Hugo Fabre <hfabre@MacBook-Pro-de-Hugo.local>
2024-10-14 22:20:11 +0200

    Make it a pwa

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      console.log('Service worker registered:', registration);
+    }).catch(function(error) {
+      console.log('Could not register service worker', error);
+    });
+  }
+
 var notyf = new Notyf();
 
 window.onload = function() {
---
+++ sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('fetch', function(event) {
+    // We don't need anything, the app is not made to use offline or cache but to be easy to install
+  });
